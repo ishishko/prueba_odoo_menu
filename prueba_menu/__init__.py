8f3bd5b7562973f15b071c3ad8111ae65618c0d3
Ignacio Shishko <shishkoignacio@gmail.com>
2024-06-03 14:07:47 -0300

    Iniciando modulo v1

--- prueba_menu/__init__.py
+++ prueba_menu/__init__.py
@@ -1,19 +1,2 @@
 # -*- coding: utf-8 -*-
-{
-    "name": "Comodato",
-    "summary": "Prueba de menu",
-    "description": """
-Prueba de menu odoo 17
-    """,
-    "autor": "Ignacio Shishko",
-    "website": "https://www.hitofusion.com/",
-    "category": "Sales/Sales",
-    "sequence": 160,
-    "version": "17.0.0.0.0",
-    "depends": ["base"],
-    "data": [
-    ],
-    "installable": True,
-    "application": True,
-    "license": "GPL-3",
-}
+from . import models, api, views
